66c6b55a4399732b707fc37374f3e3a71f061767
Prashant Pathak <theprash@gmail.com>
2015-07-31 07:00:32 +0100

    Moved game logic code into a new Game module.

---
+++ Game.fs
@@ -0,0 +1,45 @@
+﻿/// Contains the game logic. No IO is performed.
+module Game
+
+type Symbol = X | O
+
+type Cell = Full of Symbol | Empty
+
+type Board = Cell list
+
+type Position = int
+
+type Move = Game -> Position * Symbol
+
+and [<NoEquality>] [<NoComparison>]
+    Player =
+    {
+        symbol: Symbol
+        strategy: Game -> Move
+    }
+
+and [<NoEquality>] [<NoComparison>]
+    Game =
+    {
+        board: Board
+        players: Player * Player
+        nextPlayer: Player
+    }
+
+let updateListAt index value list =
+    List.mapi (fun i x -> if i = index then value else x) list
+
+let updateBoard symbol position board =
+    let cell = Full symbol
+    updateListAt (position - 1) cell board
+
+let cellEmpty position board =
+    List.nth board (position - 1) = Empty
+
+let playMove player position game =
+    let nextPlayer =
+        let (p1, p2) = game.players
+        if player.symbol = p1.symbol then p2 else p1
+    {game with
+        board = updateBoard player.symbol position game.board
+        nextPlayer = nextPlayer}
--- Program.fs
+++ Program.fs
@@ -1,51 +1,6 @@
-﻿open System
+﻿open Game
 
-type Symbol = X | O
-
-type Cell = Full of Symbol | Empty
-
-type Board = Cell list
-
-type Position = int
-
-type Move = Game -> Position * Symbol
-
-and [<NoEquality>] [<NoComparison>]
-    Player =
-    {
-        symbol: Symbol
-        strategy: Game -> Move
-    }
-
-and [<NoEquality>] [<NoComparison>]
-    Game =
-    {
-        board: Board
-        players: Player * Player
-        nextPlayer: Player
-    }
-
-let startingBoard : Board = List.init 9 (fun _ -> Empty)
-
-let updateListAt index value list =
-    List.mapi (fun i x -> if i = index then value else x) list
-
-let updateBoard symbol position board =
-    let cell = Full symbol
-    updateListAt (position - 1) cell board
-
-let cellEmpty position board =
-    List.nth board (position - 1) = Empty
-
-let playMove player position game =
-    let nextPlayer =
-        let (p1, p2) = game.players
-        if player.symbol = p1.symbol then p2 else p1
-    {game with
-        board = updateBoard player.symbol position game.board
-        nextPlayer = nextPlayer}
-
-let drawBoard board =
+let renderBoard board =
     let cellToString =
         function
         | _, Full X -> "X"
@@ -54,24 +9,22 @@ let drawBoard board =
 
     let characters = List.zip [1 .. 9] board |> List.map cellToString
 
-    let displayString =
-        match characters with
-        | [a; b; c; d; e; f; g; h; i] ->
-            [[a; b; c];
-             [d; e; f];
-             [g; h; i]]
-            |> List.map (String.concat " ")
-            |> List.collect (fun line -> [line + Environment.NewLine])
-            |> String.concat ""
-        | _ -> ""
-
-    printfn "%s" displayString
+    match characters with
+    | [a; b; c; d; e; f; g; h; i] ->
+        [[a; b; c];
+         [d; e; f];
+         [g; h; i]]
+        |> List.map (String.concat " ")
+        |> List.collect (fun line -> [line + "\n"])
+        |> String.concat ""
+    | _ -> ""
 
 let drawGame game =
-    drawBoard game.board
+    game.board |> renderBoard |> printfn "%s"
     game
 
 let makeGame playerXInput playerOInput =
+    let startingBoard = List.init 9 (fun _ -> Empty)
     let playerX = {symbol = X; strategy = playerXInput}
     let playerO = {symbol = O; strategy = playerOInput}
     {
@@ -93,5 +46,5 @@ let main argv =
     |> playMove p2 2 |> drawGame
     |> playMove p1 9 |> drawGame
     |> ignore
-    Console.ReadLine () |> ignore
+    System.Console.ReadLine () |> ignore
     0
