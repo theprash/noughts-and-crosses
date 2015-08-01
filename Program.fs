4e3dadece2741cca4bb7e076c46fb9081449821a
Prashant Pathak <theprash@gmail.com>
2015-08-01 16:29:11 +0100

    Added UnbeatableComputer strategy. Currently it always plays in the first available position so it is very beatable.

--- Program.fs
+++ Program.fs
@@ -3,7 +3,7 @@ open Result
 
 [<EntryPoint>]
 let main argv = 
-    let game = makeGame Human.strategy Human.strategy
+    let game = makeGame Human.strategy UnbeatableComputer.strategy
     game |> play Draw.drawGameResult |> ignore
     System.Console.ReadLine () |> ignore
     0
---
+++ UnbeatableComputer.fs
@@ -0,0 +1,6 @@
+﻿module UnbeatableComputer
+open Game
+
+let strategy board symbol =
+    let groups = groupPositionsByCell board
+    getPositions Empty groups |> Set.minElement
