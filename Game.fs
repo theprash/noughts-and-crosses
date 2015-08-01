096528428cea4d26848bd0c7acd5f8d3ede110fe
Prashant Pathak <theprash@gmail.com>
2015-08-01 15:19:59 +0100

    Collapsed extra pattern match case

--- Game.fs
+++ Game.fs
@@ -105,8 +105,7 @@ let play drawGameResult game =
         match nextGame.status with
         | InProgress ->
             loop nextGame
-        | Complete Draw
-        | Complete (Winner _) ->
+        | Complete _ ->
             nextGame
     drawGameResult (Success game)
     loop game
