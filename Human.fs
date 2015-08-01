1885f313db772c669749f6a63d850f94b26cbe24
Prashant Pathak <theprash@gmail.com>
2015-08-01 14:18:43 +0100

    Use simpler function syntax

--- Human.fs
+++ Human.fs
@@ -13,4 +13,4 @@ let getPositionFromConsole symbol =
     Draw.printSpaced ("Player " + symbolString symbol + ", Enter your move:")
     loop ()
 
-let strategy = fun _ symbol -> getPositionFromConsole symbol
+let strategy _ symbol = getPositionFromConsole symbol
