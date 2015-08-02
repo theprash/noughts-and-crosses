f8c1dda03398d2ddc794952d5b230d0dd2c14cf2
Prashant Pathak <theprash@gmail.com>
2015-08-02 10:57:47 +0100

    Added a summary of the planned strategy for the unbeatable computer.

--- UnbeatableComputer.fs
+++ UnbeatableComputer.fs
@@ -1,6 +1,17 @@
 ﻿module UnbeatableComputer
 open Game
 
+(*
+The computer strategy will be:
+ - If I have a winning move, then play it
+ - If you have a winning move, then play there to block it
+ - If I have a move that creates two possible winning moves, then play there
+ - If you have a move that creates two possible winning moves, then play there to block it
+ - Otherwise play in the lowest available position (deterministic behaviour may simplify testing)
+
+This should avoid a loss, while making a reasonable attempt to win.
+*)
+
 let strategy board symbol =
     let groups = groupPositionsByCell board
     getPositions Empty groups |> Set.minElement
