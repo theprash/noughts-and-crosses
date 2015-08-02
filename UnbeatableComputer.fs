ee2389f6306de36766e4dfa9e56a24808f145a3a
Prashant Pathak <theprash@gmail.com>
2015-08-02 16:02:53 +0100

    Computer can detect winning moves and play them or block them.

--- UnbeatableComputer.fs
+++ UnbeatableComputer.fs
@@ -12,6 +12,35 @@ The computer strategy will be:
 This should avoid a loss, while making a reasonable attempt to win.
 *)
 
-let strategy board symbol =
+let winningMove board symbol groups =
+    let fullPositions = getPositions (Full symbol) groups
+    let emptyPositions = getPositions Empty groups
+    lines
+    |> List.map (fun line -> Set.difference line fullPositions) // remove the full positions
+    |> List.filter (Set.count >> ((=) 1)) // keep those with one position left
+    |> List.filter (Set.isSuperset emptyPositions) // keep those that are for empty positions
+    |> List.collect Set.toList
+    |> List.sort
+    |> List.tryPick Some
+
+let myWinningMove board mySymbol groups = winningMove board mySymbol groups
+
+let yourWinningMove board mySymbol groups =
+    let yourSymbol = match mySymbol with X -> O | O -> X
+    winningMove board yourSymbol groups
+
+let lowestAvailable board mySymbol groups =
+    getPositions Empty groups |> Set.minElement |> Some
+
+let bestMoves board mySymbol groups =
+    seq {
+        yield myWinningMove board mySymbol groups
+        yield yourWinningMove board mySymbol groups
+        yield lowestAvailable board mySymbol groups
+    }
+    |> Seq.choose id
+
+let strategy board mySymbol =
     let groups = groupPositionsByCell board
-    getPositions Empty groups |> Set.minElement
+    bestMoves board mySymbol groups
+    |> Seq.pick Some
